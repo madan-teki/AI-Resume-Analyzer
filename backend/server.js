00671bf3ddeed8e8278e93e4d4dae7dde2a50c3d
madan-teki <seshumadan7156@gmail.com>
2026-02-15 11:48:04 +0530

    feat: setup express backend server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Resume Analyzer API running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
